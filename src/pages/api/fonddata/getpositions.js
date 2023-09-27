e0fcfb5e777716f524ccc10fa12873c06c46160a
Aksel Fosaas <akselfosaas@dhcp-10-22-23-220.wlan.ntnu.no>
2023-09-27 10:50:30 +0200

    added getpositions endpoint

---
+++ src/pages/api/fonddata/getpositions.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+    const portfolioId = process.env.SHAREVILLE_PORTFOLIO_ID;
+    const fetchlink = `https://www.shareville.no/api/v1/portfolios/${portfolioId}/positions`;
+
+    try {
+    const data = await fetch(fetchlink).then(res => res.json());
+    if (data){
+        res.status(200).json({message: data});
+    } else (
+        res.status(404).json( {error: `Data not found`})
+    )
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+}
