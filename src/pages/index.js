0a0b2fcf49a88278519e11ccdd003727df819dbf
Aksel Fosaas <akselfosaas@dhcp-10-22-6-118.wlan.ntnu.no>
2023-09-28 11:48:07 +0200

    created graph component, performancedisplay

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import PerformanceDisplay from "@/components/home/PerformanceDisplay"
 export default function IndexPage() {
     return (
       <div>
-        <PerformanceDisplay />
+       
       </div>
     )
   }
