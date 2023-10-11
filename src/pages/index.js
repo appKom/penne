4ac58ade9924ce9a59492a3e034cf7fa8c1608e4
linnerudsander <sandesl@stud.ntnu.no>
2023-10-11 10:47:21 +0200

    new design on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ import ScrollDownIcon from "@/components/home/ScrollDownIcon";
 export default function IndexPage() {
   return (
     <div className="h-full flex flex-col justify-center items-center">
-      <div className="w-full h-20 mb-5">
-        <Navbar img={onlineLogo.src}/>
+      <div className="w-full h-20 mb-5 sticky top-0 z-20 bg-[#131620]">
+        <Navbar img={onlineLogo.src} />
       </div>
       <div className="relative mb-[500px] w-full">
         <Splash img={rf.src}/>
