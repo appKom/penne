e492fa588c516e9a99a4f08ff7937ca1da0ef3e5
linnerudsander <sandesl@stud.ntnu.no>
2023-10-15 13:28:51 +0200

    change tailwind to css

--- src/components/all/Navbar.jsx
+++ src/components/all/Navbar.jsx
@@ -1,12 +1,14 @@
+import styles from "./Navbar.module.css"
+
 export const Navbar = ({img}) => {
     return(
-        <div className="h-full w-full flex justify-between items-center p-5">
-            <img src={img} alt="Logo" className="w-36 h-full"/>
-            <div className="flex space-x-20 mx-auto">
-                <p className="text-white font-roboto text-lg pt-1">Om oss</p>
-                <p className="text-white font-roboto text-lg pt-1">Søknader</p>
+        <div className={styles.navbar}>
+            <img src={img} alt="Logo" className={styles.logo}/>
+            <div className={styles.menu}>
+                <p className={styles.menuItem}>Om oss</p>
+                <p className={styles.menuItem}>Søknader</p>
             </div>
-            <div className="w-36 h-full"></div>
+            <div className={styles.spacer}></div>
         </div>
     )
 }
