a9f285b352ce3442f3a82ab2c45fd4efb94b38dd
Amy Andrea Sitte <amyas@stud.ntnu.no>
2023-10-29 14:05:57 +0100

    Endring accordionheader.

--- src/components/about_us/Accordion.jsx
+++ src/components/about_us/Accordion.jsx
@@ -8,7 +8,8 @@ export default function Accordion({ title, content }) {
     return (
         <div className={styles.accordion}>
             <div className={styles.accordionHeader} onClick={() => setIsOpen(!isOpen)}>
-                {title}
+                <div className={styles.accordionTitle}>{title}</div>
+                
                 <span>{isOpen ? '˄' : '˅'}</span>
             </div>
             {isOpen && <div className={styles.accordionContent}>    {content.map((element) => {
