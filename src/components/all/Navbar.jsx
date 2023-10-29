a8816e6f1a6c7fbed324f31988f34c9077c9870d
Aksel Fosaas <akselsf@stud.ntnu.no>
2023-10-29 14:44:02 +0100

    Temporary responsive navbar, needs restylinggit add *git add *

--- src/components/all/Navbar.jsx
+++ src/components/all/Navbar.jsx
@@ -1,26 +1,31 @@
 import Link from "next/link"
 import styles from "./navbar.module.css"
 import React, { useState, useEffect, useRef, useCallback } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 
-export const Navbar = ({img, bekk}) => {
-    const [showNavbar, setShowNavbar] = useState(styles.navbar_box);
-    const [lastScroll, setLastScroll] = useState(0);
+export const Navbar = ({ img, bekk }) => {
+  const [showNavbar, setShowNavbar] = useState(styles.navbar_box);
+  const [lastScroll, setLastScroll] = useState(0);
+  const [showNavMenu, setShowNavMenu] = useState(false);
 
 
-    const controlNavbar = () => {
-        if (typeof window !== 'undefined') { 
-          if (lastScroll - window.scrollY < 0) { // if scroll down hide the navbar
-            setShowNavbar(styles.hidden); 
-          } else if (lastScroll - window.scrollY > 0) { // if scroll up show the navbar
-            setShowNavbar(styles.navbar_box);  
-          }
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') {
+      if (lastScroll - window.scrollY < 0) { // if scroll down hide the navbar
+        setShowNavbar(styles.hidden);
+      } else if (lastScroll - window.scrollY > 0) { // if scroll up show the navbar
+        setShowNavbar(styles.navbar_box);
+      }
 
-          // remember current page location to use in the next move
-      setLastScroll(window.scrollY); 
+      // remember current page location to use in the next move
+      setLastScroll(window.scrollY);
     }
   };
+  const handleToggleShowNavMenu = () => {
+    setShowNavMenu(!showNavMenu);
+  }
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -33,21 +38,30 @@ export const Navbar = ({img, bekk}) => {
     }
   }, [lastScroll]);
 
-    return(
-      <div className={showNavbar}>
-        <nav className='navbar'>
-            <div className={styles.navbar}>
-                <Link href={"/"}><img src={img} alt="Logo" className={styles.logo}/></Link>
-                <div className={styles.menu}>
-                    <Link href={"/omoss"} className={styles.menuItem}>Om oss</Link>
-                    <Link href={"/soknad"} className={styles.menuItem}>Søknader</Link>
-                    <a href={"https://online.ntnu.no"} className={styles.menuItem}>Online.ntnu.no</a>
-                </div>
-                <div className={styles.spacer}></div>
-                <img src={bekk} alt="Bekk logo" className={styles.logo} />
+  return (
+    <div className={showNavbar}>
+      <nav className='navbar'>
+        <div className={styles.navbar}>
+          <Link href={"/"}><img src={img} alt="Logo" className={styles.logo} /></Link>
+          <div className={styles.menu}>
+            <Link href={"/omoss"} className={styles.menuItem}>Om oss</Link>
+            <Link href={"/soknad"} className={styles.menuItem}>Søknader</Link>
+            <a href={"https://online.ntnu.no"} className={styles.menuItem}>Online.ntnu.no</a>
+          </div>
+          <div className={styles.mobilemenucontainer}>
+            <MenuIcon onClick={() => handleToggleShowNavMenu()} className={styles.menuicon} />
 
+            <div className={styles.mobilemenu} style={{ display: showNavMenu ? "flex" : "none" }}>
+              <Link href={"/omoss"} className={styles.menuItem}>Om oss</Link>
+              <Link href={"/soknad"} className={styles.menuItem}>Søknader</Link>
+              <a href={"https://online.ntnu.no"} className={styles.menuItem}>Online.ntnu.no</a>
             </div>
-        </nav>
-      </div>
-    )
+          </div>
+          <div className={styles.spacer}></div>
+          <img src={bekk} alt="Bekk logo" className={styles.logo + " " + styles.bekklogo} />
+
+        </div>
+      </nav>
+    </div>
+  )
 }
