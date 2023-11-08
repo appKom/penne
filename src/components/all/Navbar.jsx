cb46ddcc2a7f9e0313e32e3315a21548511ca0c8
Aksel Skullerud Fosaas <63968427+akselsf@users.noreply.github.com>
2023-11-08 11:30:55 +0100

    Responsive navbar (#28)
    * Temporary responsive navbar, needs restylinggit add *git add *
    
    * increase max-size for navbar to change
    
    * change order
    
    * endring av størrelse
    
    * endring navbar
    
    * changed dropdownmenu
    
    * click outside closes navmenu
    
    ---------
    
    Co-authored-by: dinamst <dinamst@stud.ntnu.no>

--- src/components/all/Navbar.jsx
+++ src/components/all/Navbar.jsx
@@ -1,26 +1,49 @@
 import Link from "next/link"
 import styles from "./navbar.module.css"
 import React, { useState, useEffect, useRef, useCallback } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Spiral as Hamburger } from 'hamburger-react';
 
 
 
-export const Navbar = ({img, bekk}) => {
-    const [showNavbar, setShowNavbar] = useState(styles.navbar_box);
-    const [lastScroll, setLastScroll] = useState(0);
+export const Navbar = ({ img, bekk }) => {
+  const [showNavbar, setShowNavbar] = useState(styles.navbar_box);
+  const [lastScroll, setLastScroll] = useState(0);
+  const [showNavMenu, setShowNavMenu] = useState(false);
+
+  const dropdownRef = useRef(null)
+  
 
 
-    const controlNavbar = () => {
-        if (typeof window !== 'undefined') { 
-          if (lastScroll - window.scrollY < 0) { // if scroll down hide the navbar
-            setShowNavbar(styles.hidden); 
-          } else if (lastScroll - window.scrollY > 0) { // if scroll up show the navbar
-            setShowNavbar(styles.navbar_box);  
-          }
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') {
+      if (lastScroll - window.scrollY < 0) { // if scroll down hide the navbar
+        setShowNavbar(styles.hidden);
+      } else if (lastScroll - window.scrollY > 0) { // if scroll up show the navbar
+        setShowNavbar(styles.navbar_box);
+      }
 
-          // remember current page location to use in the next move
-      setLastScroll(window.scrollY); 
+      // remember current page location to use in the next move
+      setLastScroll(window.scrollY);
     }
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+        if (
+            dropdownRef.current &&
+            !dropdownRef.current.contains(event.target)
+        ) {
+            setShowNavMenu(false)
+        }
+    }
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+        document.removeEventListener('mousedown', handleClickOutside)
+    }
+}, [])
+
+  
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
@@ -33,21 +56,34 @@ export const Navbar = ({img, bekk}) => {
     }
   }, [lastScroll]);
 
-    return(
-      <div className={showNavbar}>
-        <nav className='navbar'>
-            <div className={styles.navbar}>
-                <Link href={"/"}><img src={img} alt="Logo" className={styles.logo}/></Link>
-                <div className={styles.menu}>
-                    <Link href={"/omoss"} className={styles.menuItem}>Om oss</Link>
-                    <Link href={"/soknad"} className={styles.menuItem}>Søknader</Link>
-                    <a href={"https://online.ntnu.no"} className={styles.menuItem}>Online.ntnu.no</a>
-                </div>
-                <div className={styles.spacer}></div>
-                <img src={bekk} alt="Bekk logo" className={styles.logo} />
-
+  return (
+    <div className={showNavbar}>
+      <nav className='navbar'>
+        <div className={styles.navbar}>
+        <div className={styles.mobilemenucontainer}>
+            <Hamburger toggled={showNavMenu} toggle={setShowNavMenu}></Hamburger>
+            <div ref={dropdownRef} className={styles.mobilemenu} style={{ display: showNavMenu ? "flex" : "none" }}>
+              <Link href={"/omoss"} className={styles.menuItem}>OM OSS</Link>
+              <Link href={"/soknad"} className={styles.menuItem}>SØKNAD</Link>
+              <a href={"https://online.ntnu.no"} className={styles.menuItem}>OW</a>
             </div>
-        </nav>
-      </div>
-    )
+          </div>
+          <div id={styles.online}>
+          <Link href={"/"}><img src={img} alt="Logo" className={styles.logo} /></Link>
+          </div>
+          <div className={styles.menu}>
+            <Link href={"/omoss"} className={styles.menuItem}>OM OSS</Link>
+            <Link href={"/soknad"} className={styles.menuItem}>SØKNAD</Link>
+            <a href={"https://online.ntnu.no"} className={styles.menuItem}>OW</a>
+          </div>
+         
+          <div className={styles.spacer}></div>
+          <div id={styles.bekk}>
+          <img src={bekk} alt="Bekk logo" className={styles.logo + " " + styles.bekklogo} />
+          </div>
+
+        </div>
+      </nav>
+    </div>
+  )
 }
