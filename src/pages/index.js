6f02ecb033930c860618cc4a02a8182ee888c033
Aksel Fosaas <akselsf@stud.ntnu.no>
2023-11-15 11:30:12 +0100

    fixed issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,9 +27,8 @@ export default function IndexPage() {
       </div>
       <h1 id={style.overskrift}> Online Fondet </h1>
       <div className="w-3/4 mb-10 text-lg text-justify">
-        <p className="">
-            Et fond er en kollektiv invistering der flere går sammen å plasserer penger i verdipapirmarkede. Dette kalles ofte for kollektiv sparing, og man kan se for seg at et fond er en kurv med investeringer. Online har et slikt fond, dette betyr at ulike parter innvesterer i Online, noe vi setter stor pris på. Under finner du en graf, denne grafen er en oversikt over utviklingen av Online sitt fond. Grafen inkluderer også en graf for OSEBX, som er hovedindeksen på Oslo Børs. Disse grafene kan sammenlignes for å se hvordan Online sitt fond har utviklet seg i forhold til hovedindeksen på Oslo Børs.</p>
-        <div id={style.under}>Under finner du en graf, denne grafen er en oversikt over den relative utviklingen av Online sitt fond</div> 
+        <p className="">Et fond er en kollektiv invistering der flere går sammen å plasserer penger i verdipapirmarkede. Dette kalles ofte for kollektiv sparing, og man kan se for seg at et fond er en kurv med investeringer. Online har et slikt fond, dette betyr at ulike parter innvesterer i Online, noe vi setter stor pris på. Under finner du en graf, denne grafen er en oversikt over utviklingen av Online sitt fond. Grafen inkluderer også en graf for OSEBX, som er hovedindeksen på Oslo Børs. Disse grafene kan sammenlignes for å se hvordan Online sitt fond har utviklet seg i forhold til hovedindeksen på Oslo Børs. Under finner du en graf, denne grafen er en oversikt over den relative utviklingen til Online sitt fond.
+        </p>
         <br></br>
       </div>
       <PerformanceDisplay />
