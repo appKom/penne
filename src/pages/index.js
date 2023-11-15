12b06d4fa542a9934df4509708e174fc1816b8af
Aksel Fosaas <akselsf@stud.ntnu.no>
2023-11-15 11:23:54 +0100

    fixed issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,8 +28,8 @@ export default function IndexPage() {
       <h1 id={style.overskrift}> Online Fondet </h1>
       <div className="w-3/4 mb-10 text-lg text-justify">
         <p className="">
-            Et fond er en kollektiv invistering der flere går sammen å plasserer penger i verdipapirmarkede. Dette kalles ofte for kollektiv sparing, og man kan se for seg at et fond er en kurv med investeringer. Online har et slikt fond, dette betyr at ulike parter innvesterer i Online, noe vi setter stor pris på🤍
-            Under finner du en graf, denne grafen er en oversikt over utviklingen av Online sitt fond. 
+            Et fond er en kollektiv invistering der flere går sammen å plasserer penger i verdipapirmarkede. Dette kalles ofte for kollektiv sparing, og man kan se for seg at et fond er en kurv med investeringer. Online har et slikt fond, dette betyr at ulike parter innvesterer i Online, noe vi setter stor pris på.
+            Under finner du en graf, denne grafen er en oversikt over utviklingen av Online sitt fond. Grafen inkluderer også en graf for OSEBX, som er hovedindeksen på Oslo Børs. Disse grafene kan sammenlignes for å se hvordan Online sitt fond har utviklet seg i forhold til hovedindeksen på Oslo Børs.
         </p>
         <br></br>
       </div>
--- src/pages/soknad.js
+++ src/pages/soknad.js
@@ -19,6 +19,8 @@ export default function SoknadPage() {
       <HorizontalLine />
       <SubTitle title="Hvordan søke om støtte?" />
       <Paragraph text="Alle Onlines medlemmer kan søke Onlines fond om penger. Søknaden skal være velbegrunnet og ha som hensikt å komme flest mulig medlemmer av Online til gode. For at Fondstyre skal kunne ta en god avgjørelse trenger vi en helhetlig forståelse av hvem dere er og hva dere gjør. Skriv gjerne litt for mye, enn litt for lite. Søknaden er kun gyldig dersom det søkes om mellom 10 000 kr og 100 000 kr."/>
+      <Paragraph text="Søknader sendes til fond@onlne.ntnu.no"/>
+      
       <Paragraph text="Dette skal være med i søknaden:"/>
       <ul className={styles.ul} >
           <li>Beskriv dere selv</li>
