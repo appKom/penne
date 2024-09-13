ff0254755aaa25413daa3aeb6a10e299fc6bbed5
Julian <julianao@stud.ntnu.no>
2024-09-13 23:00:59 +0200

    paragraph

--- src/components/appliance/Paragraph.tsx
+++ src/components/appliance/Paragraph.tsx
@@ -1,5 +1,3 @@
-import styles from './Paragraph.module.css';
-
-export const Paragraph = (props: { text: string }) => {
-  return <p className={styles.paragraph}>{props.text}</p>;
-};
+export const Paragraph = (props: { text: string }) => (
+  <p className="mt-10 text-xl leading-9">{props.text}</p>
+);
