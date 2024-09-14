5e0c2845f08a394f77a7725ab8e8be57c0c679ed
Julian <julianao@stud.ntnu.no>
2024-09-14 11:25:21 +0200

    members

--- src/components/about_us/Members.tsx
+++ src/components/about_us/Members.tsx
@@ -1,10 +1,12 @@
-import styles from './Members.module.css';
-
 const Member = (props: { path: string; name: string }) => {
   return (
-    <div className={styles.memberWrapper}>
-      <img src={props.path} className={styles.circleimage} />
-      <p className={styles.memberName}> {props.name}</p>
+    <div className="flex flex-col items-center justify-center gap-4 text-center w-fit">
+      <img
+        src={props.path}
+        alt={props.name}
+        className="object-cover w-64 rounded-full aspect-square"
+      />
+      <p className="ml-5 text-xl">{props.name}</p>
     </div>
   );
 };
