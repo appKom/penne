2c7c9e91aae361ea93ba04d44b7f9abfbd91d3c3
Julian <julianao@stud.ntnu.no>
2024-09-14 11:50:27 +0200

    om oss

---
+++ src/content.ts
@@ -0,0 +1,105 @@
+export const aboutUsText =
+  'Onlinefondet er en engasjert og livlig studentorganisasjon ved NTNU, dedikert til å fremme studentenes interesser og berike deres akademiske og sosiale opplevelser. Gjennom vårt fond, ledet av et dedikert fondstyre, gir vi økonomisk støtte til initiativer som sikrer at medlemmenes behov og interesser blir ivaretatt. Fra å finansiere nytt musikkutstyr for vårt linjeforeningsband, til å støtte surfeturer og sosiale arrangementer, streber vi etter å gi tilbake til vår studentkommunitet. Med en rik historie og et engasjert medlemskap, er vi stolte av vår evne til å kombinere det faglige med det sosiale, og skape varige minner for alle Onlinere.';
+
+export const currentMembers = [
+  {
+    name: 'Hilmir Straumland (Leder)',
+    image: 'Fondmedlemmer/Hilmir.jpg',
+  },
+  {
+    name: 'Johanna Wilmers',
+    image: 'Fondmedlemmer/Johanna.jpg',
+  },
+  {
+    name: 'Magnus Byrkjeland',
+    image: 'Fondmedlemmer/Magnus.jpg',
+  },
+  {
+    name: 'Maiken Lie',
+    image: 'Fondmedlemmer/noimage.jpg',
+  },
+  {
+    name: 'William Andersson',
+    image: 'Fondmedlemmer/William.jpg',
+  },
+  {
+    name: 'Thea Karin Fladby',
+    image: 'Fondmedlemmer/Thea.jpg',
+  },
+  {
+    name: 'Johanne Tronstad',
+    image: 'Fondmedlemmer/noimage.jpg',
+  },
+]
+
+export const pastMembers = [
+  {
+    year: '2022',
+    members: [
+      'Henrik Horten Hegli',
+      'Anh-Kha Vo Nguyen',
+      'Hilmir Straumland',
+      'Henrik Giil Liisberg (Leder)',
+      'William Andersson',
+      'Michael Johansen',
+      'Johanne Tronstad',
+    ]
+  },
+  {
+    year: '2021',
+    members: [
+      'Milla Weium',
+      'Jonathan Brooks (Leder)',
+      'Jan Arild Brobak',
+      'Henrik Giil Liisberg',
+      'Kaja Sofie Lundgaard',
+      'Michael Johansen',
+      'Marius Enerly',
+    ]
+  },
+  {
+    year: '2020',
+    members: [
+      'Christoffer Stensrud',
+      'Nicolai Andre Dalaaker (Leder)',
+      'Jan Arild Brobak',
+      'Christian Nyvoll',
+      'Kaja Sofie Lundgaard',
+      'Christoffer Skar Lofsberg',
+      'Marius Enerly',
+    ]
+  },
+  {
+    year: '2019',
+    members: [
+      'Kaja Sofie Lundgaard',
+      'Nicolai Andre Dalaaker',
+      'Sigurd Oxaas Wie (Leder)',
+      'Christian Nyvoll',
+      'Martin Bjerke',
+      'Christoffer Skar Lofsberg',
+      'Sverre Bjørke',
+    ]
+  },
+  {
+    year: '2018',
+    members: [
+      'Christian Nyvoll',
+      'Tord Standnes',
+      'Sigurd Oxaas Wie',
+      'Sondre Widmark',
+      'Taran Ruge',
+      'Henning Wold',
+      'Sverre Bjørke',
+    ]
+  },
+  {
+    year: '2017',
+    members: [
+      'Christoffer Skar Lofsberg',
+      'Tord Standnes',
+      'Sondre Widmark',
+      'Henning Wold',
+    ]
+  }
+]
--- src/pages/omoss.tsx
+++ src/pages/omoss.tsx
@@ -1,129 +1,36 @@
 import Member from '../components/about_us/Members';
-import styles from '../components/about_us/Members.module.css';
 import Accordion from '../components/about_us/Accordion';
 import Title from '../components/appliance/Title';
+import { HorizontalLine } from '../components/appliance/HorizontalLine';
+import { aboutUsText, currentMembers, pastMembers } from '../content';
 
 export default function OmOssPage() {
   return (
     <div>
-      <div id={styles.titel2}>
-        <div className={styles.omFondet}>
-          <div className={styles.omOssWrapper}>
-            <Title title="Om Oss" />
-            <hr className={styles.horizontalline}></hr>
-            <p>
-              Onlinefondet er en engasjert og livlig studentorganisasjon ved
-              NTNU, dedikert til å fremme studentenes interesser og berike deres
-              akademiske og sosiale opplevelser. Gjennom vårt fond, ledet av et
-              dedikert fondstyre, gir vi økonomisk støtte til initiativer som
-              sikrer at medlemmenes behov og interesser blir ivaretatt. Fra å
-              finansiere nytt musikkutstyr for vårt linjeforeningsband, til å
-              støtte surfeturer og sosiale arrangementer, streber vi etter å gi
-              tilbake til vår studentkommunitet. Med en rik historie og et
-              engasjert medlemskap, er vi stolte av vår evne til å kombinere det
-              faglige med det sosiale, og skape varige minner for alle Onlinere.
-            </p>
-          </div>
+      <div className="text-[40px] text-center">
+        <div className="relative w-full p-12 m-auto md:w-4/5">
+          <Title title="Om Oss" />
+          <HorizontalLine />
+          <p className="overflow-hidden text-xl leading-9 text-left">
+            {aboutUsText}
+          </p>
         </div>
 
-        <div className={styles.styremedlemmer}>
-          <Member
-            path={'Fondmedlemmer/Hilmir.jpg'}
-            name={'Hilmir Straumland (Leder)'}
-          />
-          <Member path={'Fondmedlemmer/Johanna.jpg'} name={'Johanna Wilmers'} />
-          <Member
-            path={'Fondmedlemmer/Magnus.jpg'}
-            name={'Magnus Byrkjeland'}
-          />
-          <Member path={'Fondmedlemmer/noimage.jpg'} name={'Maiken Lie'} />
-          <Member
-            path={'Fondmedlemmer/William.jpg'}
-            name={'William Andersson'}
-          />
-          <Member path={'Fondmedlemmer/Thea.jpg'} name={'Thea Karin Fladby'} />
-          <Member
-            path={'Fondmedlemmer/noimage.jpg'}
-            name={'Johanne Tronstad'}
-          />
+        <div className="flex flex-col items-center justify-center flex-grow gap-12 p-0 md:flex-row flex-nowrap md:flex-wrap md:p-12">
+          {
+            currentMembers.map((member) => (
+              <Member path={member.image} name={member.name} />
+            ))
+          }
         </div>
-        <div className={styles.tidligereMedlemmer}>
-          <h1> Tidligere medlemmer</h1>
-          <div className={styles.tidligereMedlemmerWrapper}>
-            <Accordion
-              title="2022"
-              content={[
-                'Henrik Horten Hegli',
-                'Anh-Kha Vo Nguyen',
-                'Hilmir Straumland',
-                'Henrik Giil Liisberg (Leder)',
-                'William Andersson',
-                'Michael Johansen',
-                'Johanne Tronstad',
-              ]}
-            />
-
-            <Accordion
-              title="2021"
-              content={[
-                'Milla Weium',
-                'Jonathan Brooks (Leder)',
-                'Jan Arild Brobak',
-                'Henrik Giil Liisberg',
-                'Kaja Sofie Lundgaard',
-                'Michael Johansen',
-                'Marius Enerly',
-              ]}
-            />
-
-            <Accordion
-              title="2020"
-              content={[
-                'Christoffer Stensrud',
-                'Nicolai Andre Dalaaker (Leder)',
-                'Jan Arild Brobak',
-                'Christian Nyvoll',
-                'Kaja Sofie Lundgaard',
-                'Christoffer Skar Lofsberg',
-                'Marius Enerly',
-              ]}
-            />
-
-            <Accordion
-              title="2019"
-              content={[
-                'Kaja Sofie Lundgaard',
-                'Nicolai Andre Dalaaker',
-                'Sigurd Oxaas Wie (Leder)',
-                'Christian Nyvoll',
-                'Martin Bjerke',
-                'Christoffer Skar Lofsberg',
-                'Sverre Bjørke',
-              ]}
-            />
-
-            <Accordion
-              title="2018"
-              content={[
-                'Christian Nyvoll',
-                'Tord Standnes',
-                'Sigurd Oxaas Wie',
-                'Sondre Widmark',
-                'Taran Ruge',
-                'Henning Wold',
-                'Sverre Bjørke',
-              ]}
-            />
-
-            <Accordion
-              title="2017"
-              content={[
-                'Christoffer Skar Lofsberg',
-                'Tord Standnes',
-                'Sondre Widmark',
-                'Henning Wold',
-              ]}
-            />
+        <div className="relative w-4/6 m-auto">
+          <h1>Tidligere medlemmer</h1>
+          <div>
+            {
+              pastMembers.map((member) => (
+                <Accordion title={member.year} content={member.members} />
+              ))
+            }
           </div>
         </div>
       </div>
