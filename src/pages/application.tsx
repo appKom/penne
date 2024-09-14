d1aa069b4ccd3918691cc16f379ac77108932332
Julian <julianao@stud.ntnu.no>
2024-09-14 13:00:41 +0200

    søknad

--- src/pages/application.tsx
+++ src/pages/application.tsx
@@ -2,26 +2,32 @@ import Title from '../components/appliance/Title';
 import { HorizontalLine } from '../components/appliance/HorizontalLine';
 import SubTitle from '../components/appliance/SubTitle';
 import { Paragraph } from '../components/appliance/Paragraph';
-
-import styles from '../components/soknad/SoknadPage.module.css';
 import { applicationText } from '../content';
+
+const blah = [
+  'Beskriv dere selv',
+  'Hva midlene skal brukes til',
+  'Hvordan midlene går onlinere til gode',
+  'Aktivitetsplan',
+  'Budsjett',
+]
 
 export default function SoknadPage() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <div id={styles.soknadWrapper}>
+      <div className='w-full p-12 sm:w-4/5'>
         <Title title="Søke om støtte" />
         <HorizontalLine />
         <SubTitle title="Hvordan søke om støtte?" />
         <Paragraph text={applicationText} />
         <Paragraph text="Søknader sendes til fondet@online.ntnu.no" />
         <Paragraph text="Dette skal være med i søknaden:" />
-        <ul className={styles.ul}>
-          <li>Beskriv dere selv</li>
-          <li>Hva midlene skal brukes til</li>
-          <li>Hvordan midlene går onlinere til gode</li>
-          <li>Aktivitetsplan</li>
-          <li>Budsjett</li>
+        <ul className="mt-8">
+          {
+            blah.map((item) => (
+              <li className="mx-8 my-4 text-xl list-disc sm:mx-12 md:mx-16">{item}</li>
+            ))
+          }
         </ul>
       </div>
     </div>
