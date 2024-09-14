d079454b326380d1f245ae57ed4529ba89ca2244
Julian <julianao@stud.ntnu.no>
2024-09-14 16:51:37 +0200

    Refactored navbar to tailwind

--- src/components/all/Navbar.tsx
+++ src/components/all/Navbar.tsx
@@ -1,103 +1,100 @@
 import { Link } from 'react-router-dom';
-import styles from './navbar.module.css';
 import { useState, useEffect, useRef } from 'react';
 import { Spiral as Hamburger } from 'hamburger-react';
+import clsx from 'clsx';
+
+const navLinks = [
+  { title: 'FONDET', path: '/' },
+  { title: 'OM OSS', path: '/omoss' },
+  { title: 'SØKNAD', path: '/soknad' },
+];
 
 const Navbar = () => {
-  const [showNavbar, setShowNavbar] = useState(styles.navbar_box);
+  const [showNavbar, setShowNavbar] = useState(true);
   const [lastScroll, setLastScroll] = useState(0);
   const [showNavMenu, setShowNavMenu] = useState(false);
 
-  const dropdownRef = useRef(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const controlNavbar = () => {
-    if (typeof window !== 'undefined') {
-      if (lastScroll - window.scrollY < 0) {
-        // if scroll down hide the navbar
-        setShowNavbar(styles.hidden);
-      } else if (lastScroll - window.scrollY > 0) {
-        // if scroll up show the navbar
-        setShowNavbar(styles.navbar_box);
-      }
+    const currentScroll = window.scrollY;
 
-      // remember current page location to use in the next move
-      setLastScroll(window.scrollY);
-    }
+    setShowNavbar(currentScroll <= lastScroll);
+    setLastScroll(currentScroll);
   };
 
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
         setShowNavMenu(false);
       }
     };
+
     document.addEventListener('mousedown', handleClickOutside);
+    window.addEventListener('scroll', controlNavbar);
+
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
+      window.removeEventListener('scroll', controlNavbar);
     };
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('scroll', controlNavbar);
-
-      // cleanup function
-      return () => {
-        window.removeEventListener('scroll', controlNavbar);
-      };
-    }
   }, [lastScroll]);
 
   return (
-    <div className={showNavbar}>
-      <nav className="navbar">
-        <div className={styles.navbar}>
-          <div ref={dropdownRef} className={styles.mobilemenucontainer}>
-            <Hamburger
-              toggled={showNavMenu}
-              toggle={setShowNavMenu}
-            ></Hamburger>
-            <div
-              ref={dropdownRef}
-              className={styles.mobilemenu}
-              style={{ display: showNavMenu ? 'flex' : 'none' }}
+    <div
+      className={clsx(
+        showNavbar ? 'opacity-100' : 'opacity-0 pointer-events-none',
+        'bg-[#131620] top-0 sticky w-full h-20 transition z-20 flex items-center justify-between p-5 text-xl',
+      )}
+    >
+      {/* MOBILE HAMBURGER MENU */}
+      <div ref={dropdownRef} className="md:hidden">
+        <Hamburger toggled={showNavMenu} toggle={setShowNavMenu} />
+        <div
+          className={`absolute top-full left-0 right-0 flex flex-col bg-[#191919] border-t border-b border-gray-200 ${showNavMenu ? 'block' : 'hidden'}`}
+        >
+          {navLinks.map((link) => (
+            <Link
+              to={link.path}
+              className="block px-8 py-4 hover:bg-white transition hover:text-[#131620]"
+              key={link.title}
             >
-              <Link to={'/'} className={styles.menuItem}>
-                FONDET
-              </Link>
-              <Link to={'/omoss'} className={styles.menuItem}>
-                OM OSS
-              </Link>
-              <Link to={'/soknad'} className={styles.menuItem}>
-                SØKNAD
-              </Link>
-            </div>
-          </div>
-          <div id={styles.online}>
-            <Link to={'/'}>
-              <img src="Online_hvit.png" alt="Logo" className={styles.logo} />
+              {link.title}
             </Link>
-          </div>
-          <div className={styles.menu}>
-            <Link to={'/'} className={styles.menuItem}>
-              FONDET
-            </Link>
-            <Link to={'/omoss'} className={styles.menuItem}>
-              OM OSS
-            </Link>
-            <Link to={'/soknad'} className={styles.menuItem}>
-              SØKNAD
-            </Link>
-          </div>
-          <div id={styles.bekk}>
-            <img
-              src="Bekk_navnetrekk_hvit.svg"
-              alt="Bekk logo"
-              className={styles.logo + ' ' + styles.bekklogo}
-            />
-          </div>
+          ))}
         </div>
-      </nav>
+      </div>
+
+      <Link to="/">
+        <img src="Online_hvit.png" alt="Logo" className="h-auto w-36" />
+      </Link>
+
+      {/* DESKTOP NAVBAR */}
+      <div className="justify-center flex-grow hidden md:flex">
+        {navLinks.map((link) => (
+          <Link
+            to={link.path}
+            className="px-8 py-4 hover:text-[#FAB759] transition"
+            key={link.title}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </div>
+
+      <Link
+        to="https://www.bekk.no/"
+        target="_blank"
+        className="hidden md:block"
+      >
+        <img
+          src="Bekk_navnetrekk_hvit.svg"
+          alt="Bekk logo"
+          className="h-auto w-36"
+        />
+      </Link>
     </div>
   );
 };
--- src/components/home/Splash.tsx
+++ src/components/home/Splash.tsx
@@ -1,13 +1,11 @@
 const Splash = () => (
-  <div className="relative flex items-center justify-center mb-[500px] w-full">
-    <div className="flex items-center justify-center max-w-full">
-      <img
-        src="realfagsbygget.png"
-        alt="Realfagsbygget"
-        className="w-11/12 animate-fadeIn"
-      />
-    </div>
-    <h1 className="text-[150px] tracking-wider absolute font-playfair animate-spashSlideUp w-min leading-snug">
+  <div className="relative flex items-center justify-center w-full h-screen mb-32 -mt-20">
+    <img
+      src="realfagsbygget.png"
+      alt="Realfagsbygget"
+      className="w-4/5 animate-fadeIn"
+    />
+    <h1 className="text-[150px] tracking-wider absolute font-playfair animate-spashSlideUp w-min leading-tight [text-shadow:_1px_1px_2px_rgb(0_0_0_/_40%)]">
       ONLINE FONDET
     </h1>
   </div>
