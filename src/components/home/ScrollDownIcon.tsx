88aea9d7faba08cf4b7f1c8061adce7ced208d34
Julian <julianao@stud.ntnu.no>
2024-09-17 12:26:21 +0200

    Scrolldown icon is clickable

--- src/components/home/ScrollDownIcon.tsx
+++ src/components/home/ScrollDownIcon.tsx
@@ -20,14 +20,20 @@ const ScrollDownIcon = () => {
     };
   }, []); // Tom dependency array betyr at effekten kjøres ved mount og cleanup ved unmount
 
+  const handleClick = (targetId: string) => {
+    const targetElement = document.getElementById(targetId);
+    targetElement?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div
       className={clsx(
-        'flex justify-center items-center h-[10px] sticky bottom-10',
+        'flex justify-center items-center h-[10px] sticky bottom-10 cursor-pointer w-16 m-auto p-5 group',
         !isVisible && 'hidden',
       )}
+      onClick={() => handleClick('home-text')}
     >
-      <span className="border-solid border-white border-r-[5px] border-b-[5px] p-[6px] animate-bounceArrow"></span>
+      <span className="border-solid border-white group-hover:border-gray-500 transition border-r-[5px] border-b-[5px] p-[6px] animate-bounceArrow"></span>
     </div>
   );
 };
