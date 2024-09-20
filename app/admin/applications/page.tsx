96dc1aadd01f813ec2c0c188d908d56a94a2b7a9
fredrir <fhansteen@gmail.com>
2024-09-20 11:42:00 +0200

    add all admin pages

--- app/admin/applications/page.tsx
+++ app/admin/applications/page.tsx
@@ -2,16 +2,16 @@
 import Custom404 from '@/app/not-found';
 import { useSession } from 'next-auth/react';
 
-const PortfolioPage = () => {
+const ApplicationsPage = () => {
   const { data: session } = useSession();
 
   if (!session || session.user?.role != 'admin') return <Custom404 />;
 
   return (
     <div className="flex flex-col min-h-screen items-center pt-8">
-      <h1 className="text-4xl">Portfølje</h1>
+      <h1 className="text-4xl">Søknader</h1>
     </div>
   );
 };
 
-export default PortfolioPage;
+export default ApplicationsPage;
--- app/admin/members/page.tsx
+++ app/admin/members/page.tsx
@@ -2,16 +2,16 @@
 import Custom404 from '@/app/not-found';
 import { useSession } from 'next-auth/react';
 
-const PortfolioPage = () => {
+const MembersPage = () => {
   const { data: session } = useSession();
 
   if (!session || session.user?.role != 'admin') return <Custom404 />;
 
   return (
     <div className="flex flex-col min-h-screen items-center pt-8">
-      <h1 className="text-4xl">Portfølje</h1>
+      <h1 className="text-4xl">Medlemmer</h1>
     </div>
   );
 };
 
-export default PortfolioPage;
+export default MembersPage;
