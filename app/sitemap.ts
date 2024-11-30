1956c43714ac63f2a7e4fd836a401e08775e9e74
fredrir <fhansteen@gmail.com>
2024-11-30 13:57:35 +0100

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://appkom.no';
+
+  return [
+    {
+      url: baseUrl + '/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: baseUrl + '/soknad',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: baseUrl + '/styret',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ];
+}
