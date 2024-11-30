39f2af57990279e5c8118b331004e7436ba5e805
fredrir <fhansteen@gmail.com>
2024-11-30 13:59:53 +0100

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://appkom.no';
+  const baseUrl = 'https://onlinefondet.no';
 
   return [
     {
