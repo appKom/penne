3be657511f7caa12a81eddab475990a5ebfb894a
fredrir <fhansteen@gmail.com>
2025-02-24 17:08:18 +0100

    feat: add approved banner to ApplicationCard

--- components/Submissions/ApplicationCard.tsx
+++ components/Submissions/ApplicationCard.tsx
@@ -1,26 +1,46 @@
 'use client';
 
 import { formatDateNorwegian } from '@/lib/dateUtils';
-import { ApplicationType } from '@/lib/types';
-import { ChevronDownIcon } from 'lucide-react';
+import { CheckCircle, ChevronDownIcon, X } from 'lucide-react';
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import type { Application } from '@prisma/client';
 
 interface Props {
-  application: ApplicationType;
+  application: Application;
 }
 
 const ApplicationCard = ({ application }: Props) => {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <div className="bg-gradient-to-br from-gray-800 to-gray-800/80 rounded-xl overflow-hidden shadow-lg">
+    <div className="bg-gradient-to-br from-gray-800 to-gray-800/80 rounded-xl overflow-hidden shadow-lg relative shadow-gray-900/30">
+      <div className="absolute top-4 right-4 z-10">
+        <div
+          className={`px-3 py-1 rounded-full border text-sm font-medium ${
+            application.approved
+              ? 'bg-green-500 text-green-50 border-green-700'
+              : 'bg-red-500 text-red-50 border-red-700'
+          }`}
+        >
+          <span className="flex flex-row items-center gap-2">
+            {application.approved ? (
+              <CheckCircle className="size-4" />
+            ) : (
+              <X className="size-4" />
+            )}
+
+            {application.approved ? 'Godkjent' : 'Ikke godkjent'}
+          </span>
+        </div>
+      </div>
+
       <button
         className="flex flex-row justify-between items-center p-6 w-full text-left transition-colors duration-300 hover:bg-gray-800/70"
         onClick={() => setExpanded(!expanded)}
       >
         <div className="flex flex-col gap-2 items-start">
-          <h1 className="text-3xl font-bold text-white">
+          <h1 className="text-3xl font-bold mt-6 md:mt-0  text-white">
             {application.recipient}
           </h1>
           <p className="text-slate-200 font-medium">{application.purpose}</p>
@@ -29,9 +49,7 @@ const ApplicationCard = ({ application }: Props) => {
           </p>
         </div>
         <ChevronDownIcon
-          className={`w-8 h-8 text-white transform transition-transform duration-300 ${
-            expanded ? 'rotate-180' : ''
-          }`}
+          className={`w-8 h-8 text-white transform transition-transform duration-300 ${expanded ? 'rotate-180' : ''}`}
         />
       </button>
 
@@ -46,7 +64,7 @@ const ApplicationCard = ({ application }: Props) => {
               collapsed: { height: 0, opacity: 0 },
             }}
             transition={{ duration: 0.4, ease: [0.04, 0.62, 0.23, 0.98] }}
-            className="overflow-hidden  backdrop-blur-sm"
+            className="overflow-hidden backdrop-blur-sm"
           >
             <div className="p-6 text-white">
               <div className="flex flex-wrap gap-4 mb-6">
@@ -85,7 +103,7 @@ const ApplicationCard = ({ application }: Props) => {
                     />
                   ) : (
                     <img
-                      src={application.attachment}
+                      src={application.attachment || '/placeholder.svg'}
                       alt="Preview"
                       className="max-w-full h-auto rounded-lg border-2 border-gray-600"
                     />
