74efc1a3041785da3d649f0aad823f8c118aa97e
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2025-05-05 12:52:04 +0200

    Update content

--- app/styret/page.tsx
+++ app/styret/page.tsx
@@ -1,5 +1,4 @@
 import PastMembers from '@/components/about/PastMembers';
-import { aboutUsText } from '@/lib/content';
 import { MemberType } from '@/lib/types';
 import { prisma } from '@/lib/prisma';
 import CurrentMembers from '@/components/about/CurrentMembers';
@@ -19,12 +18,12 @@ const AboutPage = async () => {
         Fondstyret
       </h1>
       <SemiTitle text="Hvem er vi?" />
-      <div className="flex flex-col items-center justify-center w-full gap-6 m-auto">
-        {aboutUsText.map((paragraph) => (
-          <p key={paragraph} className="text-gray-400 md:leading-7 md:text-lg">
-            {paragraph}
-          </p>
-        ))}
+      <div className="flex flex-col items-start justify-center w-full gap-6 m-auto">
+        <Paragraph text="Onlinefondet er en gruppe i linjeforeningen Online. Gjennom vårt fond, ledet av et dedikert Fondstyre, gir vi økonomisk støtte til initiativer som sikrer at Onlines medlemmers behov og interesser blir ivaretatt. Fra å finansiere nytt musikkutstyr for vårt linjeforeningsband, støtte ny sofa på kontoret og gi bidrag til sosiale arrangementer, streber vi etter å gi tilbake til nåværende og fremtidig medlemmer av Online." />
+        <Paragraph text="Fondstyret består til enhver tid av:" />
+        <List items={["2 Riddere av det Indre lager", "2 tidligere medlemmer av Hovedstyret", "2 onlinemedlemmer", "Økonomiansvarlig i Online"]} />
+        <Paragraph text="Medlemmene blir valgt gjennom Fondets generalforsamlingen på våren, og besitter vervet i to år. Alle medlemmer av Fondstyret har stemmerett, og Fondstyret velger sin egen leder." />
+        <Paragraph text="For øyeblikket består fondet av:" />
       </div>
       <CurrentMembers members={members} />
 
@@ -38,4 +37,16 @@ export default AboutPage;
 
 const SemiTitle = ({ text }: { text: string }) => (
   <h1 className="mb-4 text-2xl font-semibold">{text}</h1>
+);
+
+const Paragraph = ({ text }: { text: string }) => (
+  <p className="text-gray-400 md:leading-7 md:text-lg">{text}</p>
+);
+
+const List = ({ items }: { items: string[] }) => (
+  <ul className="text-gray-400 md:leading-7 md:text-lg ">
+    {items.map((item, index) => (
+      <li key={index}>- {item}</li>
+    ))}
+  </ul>
 );
--- lib/content.ts
+++ lib/content.ts
@@ -4,11 +4,14 @@ export const homeText = [
 ];
 
 export const applicationText = [
-  'Alle Onlines medlemmer kan søke Onlines fond om penger. Søknaden skal være velbegrunnet og ha som hensikt å komme flest mulig medlemmer av Online til gode. For at Fondstyre skal kunne ta en god avgjørelse trenger vi en helhetlig forståelse av hvem dere er og hva dere gjør.',
-  'Skriv gjerne litt for mye, enn litt for lite. Søknaden er kun gyldig dersom det søkes om mellom 10 000 kr og 100 000 kr.',
+  'Alle Onlines medlemmer kan søke Onlines fond om penger. Søknaden skal være velbegrunnet og ha som hensikt å komme flest mulig medlemmer av Online til gode. For at Fondstyret skal kunne ta en god avgjørelse trenger vi en helhetlig forståelse av hvem dere er og hva dere gjør.',
+  'Skriv gjerne litt for mye, enn litt for lite. Søknaden må være på minst 10 000 kr for å kunne bli behandlet av Fondstyret. Søknader på større beløp enn 100 000 kr skal behandles ekstraordinært, ref. vedtektene til Fondet.',
+  'Gjerne ta kontakt med oss i Fondstyret dersom du skulle lure på noe angående søknader til Fondet!',
 ];
 
-export const aboutUsText = [
-  'Onlinefondet er en engasjert og livlig komité i linjeforeningen Online, dedikert til å fremme informatikkstudenters interesser og berike deres akademiske og sosiale opplevelser. Gjennom vårt fond, ledet av et dedikert fondstyre, gir vi økonomisk støtte til initiativer som sikrer at medlemmenes behov og interesser blir ivaretatt.',
-  'Fra å finansiere nytt musikkutstyr for vårt linjeforeningsband til å støtte surfeturer og sosiale arrangementer, streber vi etter å gi tilbake til vår studentkommunitet. Med en rik historie og et engasjert medlemskap, er vi stolte av vår evne til å kombinere det faglige med det sosiale, og skape varige minner for alle Onlinere.',
-];
+/* export const aboutUsText = [
+  'Onlinefondet er en gruppe i linjeforeningen Online. Gjennom vårt fond, ledet av et dedikert Fondstyre, gir vi økonomisk støtte til initiativer som sikrer at Onlines medlemmers behov og interesser blir ivaretatt. Fra å finansiere nytt musikkutstyr for vårt linjeforeningsband, støtte ny sofa på kontoret og gi bidrag til sosiale arrangementer, streber vi etter å gi tilbake til nåværende og fremtidig medlemmer av Online.',
+  'Fondstyret består til enhver tid av:',
+  'Medlemmene blir valgt gjennom Fondets generalforsamlingen på våren, og besitter vervet i to år. Alle medlemmer av Fondstyret har stemmerett, og Fondstyret velger sin egen leder.',
+  'For øyeblikket består fondet av:',
+]; */
