2b78a40edd38542d71adbba6617e687be721cce0
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2025-09-10 17:03:40 +0900

    Add deadline countdown

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APPLICATION_DATES = [
+  { month: 1, day: 15 },  // 15. February
+  { month: 3, day: 15 },  // 15. April
+  { month: 8, day: 15 },  // 15. September
+  { month: 9, day: 15 },  // 15. November
+]
