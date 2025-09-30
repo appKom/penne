f1879f51e3bc77de70eef0fe2b87bad50e184438
magnusouren <magnut0203@gmail.com>
2025-09-30 17:10:04 +0200

    fix bug when reffering to november, should be 10 instead of 9

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ export const APPLICATION_DATES = [
   { month: 1, day: 15 },  // 15. February
   { month: 3, day: 15 },  // 15. April
   { month: 8, day: 15 },  // 15. September
-  { month: 9, day: 15 },  // 15. November
+  { month: 10, day: 15 },  // 15. November
 ]
